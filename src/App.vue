44eb6faf9fbfcaec466ad695a231f1baef8fb681
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2021-12-12 17:43:58 +0200

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,95 @@
 <template>
+  <header>
+    <h1>
+      <span>Header</span>
+      hgfdhgdfh
+    </h1>
+    <nav class="nav">
+      <ul class="steps noselect">
+        <li> page 1 </li>
+        <li> page 1 </li>
+        <li> page 1 </li>
+      </ul>
+    </nav>
+  </header>
   <router-view />
 </template>
+
+<script>
+
+export default {
+  name: 'app',
+}
+</script>
+
+<style lang="scss" scoped>
+
+header {
+  color: map-get($header, fc);
+  background: map-get($header, bg);
+  border-bottom: 1px solid lighten(map-get($header, bg), 16%);
+  width: 100%;
+  height: 80px;
+  position: fixed;
+  left: 0;
+  top: 0;
+
+  h1 {
+    font-size: 1.5rem;
+    font-weight: normal;
+    text-align: center;
+    width: 100%;
+    height: 64px;
+    line-height: 64px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    overflow: hidden;
+
+    span {
+      color: $fc-accent;
+      padding: .5rem;
+    }
+  }
+
+  .nav {
+    background: darken(map-get($header, bg), 6%);
+    display: flex;
+    justify-content: center;
+    height: 32px;
+    position: relative;
+
+    &-themes {
+      position: absolute;
+      right: .5rem;
+    }
+  }
+
+  .steps {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 32px;
+
+    li {
+      color: darken($fc-secondary, 12%);
+      text-align: center;
+      line-height: 2rem;
+      margin: 0 1px;
+      padding: 0 1rem;
+      position: relative;
+      cursor: pointer;
+
+      &:not(:last-child):after {
+        content: '/';
+        color: $fc-middle;
+        position: absolute;
+        right: -4px;
+      }
+
+      &.selected {
+        color: $fc-accent;
+      }
+    }
+  }
+}
+</style>
