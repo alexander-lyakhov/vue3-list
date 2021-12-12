4b072ee21f99c91fb7ecf5de8b3da6c647b02c2f
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2021-12-12 15:01:07 +0200

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+﻿//import VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+import home from '@/views/home.vue'
+
+console.log(home)
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: home
+    },
+  ]
+});
+
+export default router
---
+++ src/views/home.vue
@@ -0,0 +1,14 @@
+﻿<template>
+  <h1>Home</h1>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  background: #606060;
+  border: 1px solid #999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 4rem;
+}
+</style>
